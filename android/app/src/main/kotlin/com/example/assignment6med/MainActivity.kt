efd52bdd3e2b18ca5a5881098eb8f2d2fb089802
sonalijain1999 <kcjain1970@gmail.com>
2021-11-29 13:34:39 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/assignment6med/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assignment6med
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:assignment6med/screens/firstScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body:  FirstScreen(),
+    );
+  }
+}
